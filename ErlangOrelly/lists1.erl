c4a3cb67d4165c0944d859aae556fe54e7d9863f
Eric Muli <erickiokom@gmail.com>
2020-05-18 17:07:31 +0300

    Did a recursion with lists

---
+++ ErlangOrelly/lists1.erl
@@ -0,0 +1,20 @@
+-module(lists1).
+-export([product/1]).
+
+%public entry point
+
+product([])->0;  %in case the list is empty return 0
+product(List)->product(List,1).  %%calling the private product functions
+
+
+product([] , Product)->Product; % when list empty, stop, report
+
+%%if list is not  empty
+
+product([Head|Tail],Product)->product(Tail , (Product*Head)). 
+
+%%Small Documentation
+%%WHat this function does is that it multiplies the number in the given list by the product of the previous number i.e
+%[1,2,3,4,5,6,7]
+%1*1 = 1
+%1*2 = 2, 2 * 3 = 6 , 6*4 -= 24 , 24 *5 = 120 , 120 * 6 = 720, 720 * 7 = 5040
