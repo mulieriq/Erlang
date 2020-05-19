444778d8d5aa217a8c913a4d1c17c1385308d655
Eric Muli <erickiokom@gmail.com>
2020-05-19 15:18:19 +0300

    Docummentented lists3

--- ErlangOrelly/lists3.erl
+++ ErlangOrelly/lists3.erl
@@ -4,3 +4,7 @@ addrow(Initial) -> addrow(Initial , 0 ,[]).
 
 addrow([],0,Final) -> [ 0| Final]; 
 addrow([H|T], Last , New ) -> addrow(T,H,[Last + H | New]).
+
+
+%%this block of code takes a list i.e [0,1,0] and returns  [0,1,1,0] , creating a spefic pascals triangle row
+%from its predicessor, i.e [0,1,1,0] then [0,1,2,1,0]
