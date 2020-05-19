40d7355a906eb871a22f0d1c103f6aefb511aa06
Eric Muli <erickiokom@gmail.com>
2020-05-19 15:48:04 +0300

    High Order Functions

--- ErlangOrelly/highorderfunctions.erl
+++ ErlangOrelly/highorderfunctions.erl
@@ -1,6 +1,10 @@
 -module(highorderfunctions).
--export([hof/0]).
-hof()->0.
+-export([hof/2]).
+hof(Value,Function)-> 3 * Function(Value).
 
-%%This module deals with the cli alot
-%%No working with the files
+% 3> Fun = fun (Value)->20*Value end.
+% #Fun<erl_eval.7.126501267>
+% 4> highorderfunctions:hof(12,Fun).                        
+% 720
+% 5> 4> hof:tripler(6,fun(Value)->20*Value end).
+% 360
